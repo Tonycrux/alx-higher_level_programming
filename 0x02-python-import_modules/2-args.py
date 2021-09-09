340a5b4e6755d1f97b0e449f1e926950ce6ed6a0
Tonycrux <sunnexbanty@gmail.com>
2021-09-09 13:08:42 -0700

    2nd commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     if count == 0:
         print("0 arguments.")
     elif count == 1:
-        print("1 arguments:")
+        print("1 argument:")
     else:
         print("{} arguments:".format(count))
     for i in range(count):
