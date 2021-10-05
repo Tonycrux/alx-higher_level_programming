bc5e3c1ac85e4cc67f80fb73f14d024785c88f39
Tonycrux <sunnexbanty@gmail.com>
2021-10-04 19:04:40 -0700

    Yeah

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,62 +1,81 @@
 #!/usr/bin/python3
-"""
-nqueens backtracking program to print the coordinates of n queens
-on an nxn grid such that they are all in non-attacking positions
-"""
-
-
+"""Module is to solve the N-Queens challenge problem"""
 from sys import argv
 
+
+def checkspot(board, r, c):
+    n = len(board) - 1
+    if board[r][c]:
+        return 0
+    for row in range(r):
+        if board[row][c]:
+            return 0
+    i = r
+    j = c
+    while i > 0 and j > 0:
+        i -= 1
+        j -= 1
+        if board[i][j]:
+            return 0
+    i = r
+    j = c
+    while i > 0 and j < n:
+        i -= 1
+        j += 1
+        if board[i][j]:
+            return 0
+    return 1
+
+
+def initboard(n=4):
+    b = []
+    for r in range(n):
+        b.append([0 for c in range(n)])
+    return b
+
+
+def findsoln(board, row):
+    for col in range(len(board)):
+        if checkspot(board, row, col):
+            board[row][col] = 1
+            if row == len(board) - 1:
+                print(convtosoln(board))
+                board[row][col] = 0
+                continue
+            if findsoln(board, row + 1):
+                return board
+            else:
+                board[row][col] = 0
+    return None
+
+
+def convtosoln(board):
+    soln = []
+    n = len(board)
+    for r in range(n):
+        for c in range(n):
+            if board[r][c]:
+                soln.append([r, c])
+    return soln
+
+
+def nqueens(n=4):
+    for col in range(n):
+        board = initboard(n)
+        board[0][col] = 1
+        findsoln(board, 1)
+
+
 if __name__ == "__main__":
-    a = []
     if len(argv) != 2:
         print("Usage: nqueens N")
         exit(1)
-    if argv[1].isdigit() is False:
+    try:
+        n = int(argv[1])
+    except:
         print("N must be a number")
         exit(1)
-    n = int(argv[1])
     if n < 4:
         print("N must be at least 4")
         exit(1)
-
-    # initialize the answer list
-    for i in range(n):
-        a.append([i, None])
-
-    def already_exists(y):
-        """check that a queen does not already exist in that y value"""
-        for x in range(n):
-            if y == a[x][1]:
-                return True
-        return False
-
-    def reject(x, y):
-        """determines whether or not to reject the solution"""
-        if (already_exists(y)):
-            return False
-        i = 0
-        while(i < x):
-            if abs(a[i][1] - y) == abs(i - x):
-                return False
-            i += 1
-        return True
-
-    def clear_a(x):
-        """clears the answers from the point of failure on"""
-        for i in range(x, n):
-            a[i][1] = None
-
-    def nqueens(x):
-        """recursive backtracking function to find the solution"""
-        for y in range(n):
-            clear_a(x)
-            if reject(x, y):
-                a[x][1] = y
-                if (x == n - 1):  # accepts the solution
-                    print(a)
-                else:
-                    nqueens(x + 1)  # moves on to next x value to continue
-
-    # start the recursive process at x = 0
-    nqueens(0)
+    nqueens(n)
