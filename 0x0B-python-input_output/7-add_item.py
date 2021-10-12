733ebeb18f7879b6b994033f56a99b3614ec2399
Tonycrux <sunnexbanty@gmail.com>
2021-10-12 13:44:58 -0700

    Git

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-"""module to load and save"""
-import sys
+"""defining load_from_json_file function"""
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file =\
-        __import__('6-load_from_json_file').load_from_json_file
 
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+import json
+
+
+def load_from_json_file(filename):
+    """creates an object from json file"""
+    with open(filename, 'r', encoding="utf-8") as f:
+        return json.load(f)
