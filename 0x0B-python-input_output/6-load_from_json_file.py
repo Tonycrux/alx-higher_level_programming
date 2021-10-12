6121126ff9c7ae83243600dd98f278fd4f5de337
Tonycrux <sunnexbanty@gmail.com>
2021-10-12 13:39:23 -0700

    add

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""difining save_to_json_file function"""
+"""defining load_from_json_file function"""
 
 
 import json
 
 
-def save_to_json_file(my_obj, filename):
-    """writes an object to text file"""
-    with open(filename, 'w', encoding="utf-8") as f:
-        json.dump(my_obj, f)
+def load_from_json_file(filename):
+    """creates an object from json file"""
+    with open(filename, 'r', encoding="utf-8") as f:
+        return json.load(f)
