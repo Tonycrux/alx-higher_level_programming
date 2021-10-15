c1a61336fd0d7b31a523f15250c3e5abe1c7af4b
Tonycrux <36515560+Tonycrux@users.noreply.github.com>
2021-10-15 08:02:45 +0100

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # base.py
-# Brennan D Baraban <375@holbertonschool.com>
+# Oluyele Sunday Anthony <Sunnexbanty@gmail.com>
 """Defines a base model class."""
 import json
 import csv
