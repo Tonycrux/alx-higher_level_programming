11165e0b84d6c5b2979e37d818710e5541352085
Tonycrux <sunnexbanty@gmail.com>
2021-12-23 16:05:21 -0800

    ORM

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python3
 """
-prints all City objects from a database
+lists all City objects from a database
 """
 
 import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from relationship_state import Base, State
+from relationship_city import City
 from sys import argv
-from model_state import Base, State
-from model_city import City
 
 
 if __name__ == "__main__":
-    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+    eng = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(argv[1],
                                                                     argv[2],
                                                                     argv[3]))
     Base.metadata.create_all(eng)
     Session = sessionmaker(bind=eng)
     session = Session()
-    rows = session.query(City, State).filter(City.state_id == State.id)\
-                                     .order_by(City.id).all()
-    for city, state in rows:
-        print("{}: ({}) {}".format(state.name, city.id, city.name))
+    rows = session.query(City).all()
+    for city in rows:
+        print("{}: {} -> {}".format(city.id, city.name, city.state.name))
     session.close()
--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python3
+"""
+contains the class City
+"""
+
+import sqlalchemy
+from sqlalchemy import Column, Integer, String, ForeignKey
+from relationship_state import Base, State
+
+
+class City(Base):
+    """Representation of a city"""
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))#!/usr/bin/python3
 # Defines a City model.
 # Inherits from SQLAlchemy Base and links to the MySQL table cities.
 
