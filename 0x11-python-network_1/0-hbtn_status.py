7572f98611872786d26657730c79910b1a0d7ae8
Tonycrux <36515560+Tonycrux@users.noreply.github.com>
2022-01-08 15:01:00 +0100

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,1 +1,14 @@
-./0-hbtn_status.py | cat -e
+#!/usr/bin/python3
+"""
+a Python script that fetches https://intranet.hbtn.io/status
+"""
+import urllib.request
+
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode("UTF-8")))
