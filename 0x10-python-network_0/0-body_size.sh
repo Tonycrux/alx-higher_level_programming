d2e50515f5a735ce3e9784b266ff3b262ed2b3cf
Tonycrux <sunnexbanty@gmail.com>
2022-01-11 07:29:38 -0800

    ehn

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# prints the content length of a header
-# of a get requests using curl
-
-curl -Is "$1" |grep "Content-Length:"| cut -d' ' -f2
-
+# curl size
+curl -i $1 | grep Content-Length | tail -c 4
